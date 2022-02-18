c80f30045572453ce7d22bc9ae24e92b8c0f975a
Melissa <wolfaholic@users.noreply.github.com>
2022-02-17 22:21:36 -0800

    Saving

--- src/actions/index.js
+++ src/actions/index.js
@@ -2,6 +2,11 @@ export const ADD_ONE = "ADD_ONE";
 
 export const APPLY_NUMBER = "APPLY_NUMBER";
 export const CHANGE_OPERATION = "CHANGE_OPERATION";
+export const CLEAR_DISPLAY = "CLEAR_DISPLAY";
+export const MEMORY_PLUS = "MEMORY_PLUS";
+export const MEMORY_RETURN = "MEMORY_RETURN";
+export const MEMORY_CLEAR = "MEMORY_CLEAR";
+ 
 
 export const addOne = () => {
     return({type:ADD_ONE});
@@ -9,4 +14,25 @@ export const addOne = () => {
 
 export const applyNumber = (number) => {
     return({type:APPLY_NUMBER, payload:number});
+}
+
+export const changeOperation = (operator) => {
+    return ({type: CHANGE_OPERATION, payload:operator});
+
+}
+
+export const clearDisplay = () => {
+    return ({type: CLEAR_DISPLAY});
+}
+
+export const memoryPlus = () => {
+    return ({type: MEMORY_PLUS});
+}
+
+export const memoryReturn = () => {
+    return ({type: MEMORY_RETURN});
+}
+
+export const memoryClear = () => {
+    return ({type: MEMORY_CLEAR});
 }
